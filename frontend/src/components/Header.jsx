ff3b9051c767598fd9a416468c1985e8f0cdb27f
manoj agarwal <aryan070303@gmail.com>
2022-10-06 19:32:09 +0530

    added login and register frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,27 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
 
 function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+        
+    }
   return (
     <header className='header'>
         <div className='logo'>
             <Link to='/'>GoalSetter</Link>
         </div>
         <ul>
-            <li>
+            {(user==null || user==undefined) ? (<>
+                <li>
                 <Link to='/login'>
                     <FaSignInAlt />Login
 
@@ -21,6 +34,19 @@ function Header() {
 
                 </Link>
             </li>
+            </>
+            
+            
+            
+                ) : (
+                    <li>
+                    <button className='btn' onClick={onLogout}>
+                        <FaSignOutAlt />Logout
+    
+                    </button>
+                    </li>
+            )}
+            
         </ul>
     </header>
   )
